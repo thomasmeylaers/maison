598ffe70b6fc20fe363b7c701e610ec8bc880230
Thomas Meylaers <thomas.meylaers@gmail.com>
2021-11-20 12:07:33 +0100

    mobiele versie gefixed

--- js/index.js
+++ js/index.js
@@ -42,6 +42,21 @@ function scrollFunction() {
             $('#logo_wit').css({ 'display': 'block' })
             $('#logo_groen').css({ 'display': 'none' })
         }
+    } else {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("nav").style.backgroundColor = "white";
+            $('.nav__link').css({ 'color': 'black' });
+            $('.nav__stripe').css({ 'color': 'black' });
+            $('#logo_wit').css({ 'display': 'none' })
+            $('#logo_groen').css({ 'display': 'block' })
+        } else {
+            document.getElementById("nav").style.backgroundColor = "transparent";
+            $('.nav__link').css({ 'color': 'white' });
+            $('.page__link').css({ 'color': 'black' });
+            $('.nav__stripe').css({ 'color': 'white' });
+            $('#logo_wit').css({ 'display': 'block' })
+            $('#logo_groen').css({ 'display': 'none' })
+        }
     }
 }
 
