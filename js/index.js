c9ccd237fb3f152e42ec944ee1c69c9cc5fa6c5b
Thomas Meylaers <thomas.meylaers@gmail.com>
2022-01-01 17:33:38 +0100

    scroll erro verwijdert

--- js/index.js
+++ js/index.js
@@ -45,39 +45,39 @@ $(window).on('load', function () {
 
 
     }
-    window.onscroll = function () { scrollFunction() };
-    function scrollFunction() {
-        if (screen.width > '1024') {
-            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-                document.getElementById("nav").style.backgroundColor = "white";
-                $('.nav__link').css({ 'color': 'black' });
-                $('.nav__stripe').css({ 'color': 'black' });
-                $('#logo_wit').css({ 'display': 'none' })
-                $('#logo_groen').css({ 'display': 'block' })
-            } else {
-                document.getElementById("nav").style.backgroundColor = "transparent";
-                $('.nav__link').css({ 'color': 'white' });
-                $('.nav__stripe').css({ 'color': 'white' });
-                $('#logo_wit').css({ 'display': 'block' })
-                $('#logo_groen').css({ 'display': 'none' })
-            }
-        } else {
-            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-                document.getElementById("nav").style.backgroundColor = "white";
-                $('.nav__link').css({ 'color': 'black' });
-                $('.nav__stripe').css({ 'color': 'black' });
-                $('#logo_wit').css({ 'display': 'none' })
-                $('#logo_groen').css({ 'display': 'block' })
-            } else {
-                document.getElementById("nav").style.backgroundColor = "transparent";
-                $('.nav__link').css({ 'color': 'white' });
-                $('.page__link').css({ 'color': 'black' });
-                $('.nav__stripe').css({ 'color': 'white' });
-                $('#logo_wit').css({ 'display': 'block' })
-                $('#logo_groen').css({ 'display': 'none' })
-            }
-        }
-    }
+    // window.onscroll = function () { scrollFunction() };
+    // function scrollFunction() {
+    //     if (screen.width > '1024') {
+    //         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    //             document.getElementById("nav").style.backgroundColor = "white";
+    //             $('.nav__link').css({ 'color': 'black' });
+    //             $('.nav__stripe').css({ 'color': 'black' });
+    //             $('#logo_wit').css({ 'display': 'none' })
+    //             $('#logo_groen').css({ 'display': 'block' })
+    //         } else {
+    //             document.getElementById("nav").style.backgroundColor = "transparent";
+    //             $('.nav__link').css({ 'color': 'white' });
+    //             $('.nav__stripe').css({ 'color': 'white' });
+    //             $('#logo_wit').css({ 'display': 'block' })
+    //             $('#logo_groen').css({ 'display': 'none' })
+    //         }
+    //     } else {
+    //         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    //             document.getElementById("nav").style.backgroundColor = "white";
+    //             $('.nav__link').css({ 'color': 'black' });
+    //             $('.nav__stripe').css({ 'color': 'black' });
+    //             $('#logo_wit').css({ 'display': 'none' })
+    //             $('#logo_groen').css({ 'display': 'block' })
+    //         } else {
+    //             document.getElementById("nav").style.backgroundColor = "transparent";
+    //             $('.nav__link').css({ 'color': 'white' });
+    //             $('.page__link').css({ 'color': 'black' });
+    //             $('.nav__stripe').css({ 'color': 'white' });
+    //             $('#logo_wit').css({ 'display': 'block' })
+    //             $('#logo_groen').css({ 'display': 'none' })
+    //         }
+    //     }
+    // }
 
     // If initial position of the screen is not at the top 
     function initialFix() {
